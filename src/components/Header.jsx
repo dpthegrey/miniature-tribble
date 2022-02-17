14561c05e59bee397c22d29914e5a087af8a2918
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-17 10:47:35 +0530

    Adding Styles To A Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <div className="container">
-      <h1>{text}</h1>
-    </div>
+    <header style={headerStyles}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
   );
 }
 
 Header.defaultProps = {
   text: "Header",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 };
 
 Header.protoTypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
 
 export default Header;
