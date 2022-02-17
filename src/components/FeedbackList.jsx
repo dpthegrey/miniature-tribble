712228587010d7bd57c0354b9f736fdc09d1561d
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-17 12:08:58 +0530

    Fix: {} to () & unused import
    Signed-off-by: dpthegrey <49444980+dpthegrey@users.noreply.github.com>

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -8,9 +8,9 @@ function FeedbackList({ feedback }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        <FeedbackItem key={item.id} item={item} />;
-      })}
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
     </div>
   );
 }
